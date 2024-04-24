94fac94dfef0acd14a05efa3e131debaf33b75f7
Noel Gutierrez-Brizuela <noelgb@ldeo.columbia.edu>
2024-04-24 12:20:16 -0400

    wrote met functions

--- pwp.py
+++ pwp.py
@@ -268,22 +268,75 @@ def mix5( profile, ztop, zbot ):
                                            profile['temp'][ztop:zbot] )
     return profile 
 
+# ------------------------------------------------------------
+# ------------------- ROUTINES THAT DEAL WITH FORCING --------
+
+def prepare_forcing( met_xr ):
+    # Give variables standard names
+    met_xr = translate_met( met_xr );
+    # Now compute wind stress, heat flux, and salinity/fresh
+    met_xr['taux'], met_xr['tauy'] = get_tau( met_xr )
+    met_xr['q_in'] = met_xr['q_sw']
+    met_xr['q_out'] = met_xr['q_lw'] + met_xr['q_lat'] \
+                         + met_xr['q_sen']
+    met_xr['emp'] = np.abs( met_xr['evap'] ) - np.abs( met_xr['prec'] )
+    return met_xr 
+
+def translate_met( met_xr ):
+    # Take in met data and translate variable names so pwp 
+    # can understand. 
+
+    # Create dictionary whose keys are correct names, and 
+    # assigned are lists of alternate names
+    var_names = { 'q_sw' : ['msnswrf'], 'q_lw' : ['msnlwrf'], 
+                  'q_lat' : ['mslhf'] , 'q_sen' : ['msshf'],
+                  'prec' : ['mtpr'] , 'evap' : ['mer'] , 
+                  'u10' : ['u10'], 'v10' : ['v10'] }
+    original_keys = list( var_names.keys() )
+
+    # Cycle through variables
+    for var in original_keys: 
+        possible_names = var_names[var] 
+        # Check which of possible names is stored in met_xr
+        which_name = [ var_in_obj for var_in_obj \
+                       in list( met_xr.variables ) if \
+                       var_in_obj in possible_names ]
+        if len( which_name ) == 0 :
+            raise Exception('Variable ' + var + ' not found in xarray.' )
+        elif len( which_name ) > 1 :
+            raise Exception('Found multiple matches for variable ' + var + ' in xarray.')
+        else:
+            # Restructure var_name dict such that current name is key
+            var_names[ which_name[0] ] = var; 
+            del var_names[var] 
+
+    # Now change names of variables in xr_obj
+    xr_obj = xr_obj.rename( var_names )
+    return xr_obj     
+
+def get_tau( met_xr ):
+    # Compute wind stress using the speed-dependent drag of Large and Pond (1981)
+    # with a correction for high speeds following Powell et al (2003)
+    compvec = met_xr['u10'] + 1j*met_xr['v10']
+    speed = np.abs( compvec )
+    # Create masks for each speed category
+    slow = speed <= 11;  
+    moderate =  ( ~ slow ) * ( speed < 30 )
+    fast = speed > 30; 
+    # Set values for Cd
+    Cd = np.zeros( compvec.shape );
+    Cd[slow] = 1.2e-3;
+    Cd[moderate] = ( 0.49 + 0.065 * speed[moderate] )*1e-3
+    Cd[fast] = 1.9e-3
+    Cd = xr.DataArray( data = Cd , coords = compvec.coords )
+    # Use Cd to compute wind stress
+    taux = 1.22 * Cd * speed * met_xr['u10']
+    tauy = 1.22 * Cd * speed * met_xr['v10']
+    return taux, tauy
+    
 
 
-def stir( profile, rc, r, j):
-    # This mixes cells around j (depth-wise) to ensure that the 
-    # Ri after (r_new) is r_new > rc (stands for critical Ri)
-    # will be difficult to interpret from earlew's code
-    pass 
 
-def update_TS( ocn , met, params ):
-    # Function to implement a single timestep on ocn T,S
-    # Will return a new ocn object
-    pass 
-
-def SW_radiation( ocn, met, params ):
-    # Given shortwave flux, determine exponential profile of absorption. Might not need to know ocn state. 
-    pass 
 
 
 
